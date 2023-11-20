0b95ee80b654717af2a05c2d02a5e29fb726dfdb
Samforquer <samforquer@gmail.com>
2023-11-20 12:04:05 -0500

    Initial commit

---
+++ src/test/java/com/samforquer/graphqldotapicker/GraphqldotapickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samforquer.graphqldotapicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqldotapickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
